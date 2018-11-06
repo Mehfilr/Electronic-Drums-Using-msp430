a63960993655aa62e067f15a1f99733a36af16d5
Mehfilr <41439954+Mehfilr@users.noreply.github.com>
2018-11-06 20:57:19 +0600

    Add files via upload
    Application required to install:
    1. Serial to MIDI converter
    2. Ableton 9 Live

---
+++ arduino_drumkit_revised.ino
@@ -0,0 +1,53 @@
+#include <MIDI.h>
+#define  MAX_PIEZO  2
+
+uint8_t piezo[MAX_PIEZO] = { A0, A2,};
+uint8_t drumType[MAX_PIEZO] = { 5, 4};
+
+const int threshold = 500;  
+
+void setup() 
+{
+ for (uint8_t i=0; i<MAX_PIEZO; i++)
+   pinMode(piezo[i], INPUT);
+
+ Serial.begin(57600);      
+}
+
+void MidiTX(unsigned char StatusByte, unsigned char Note, unsigned char Val)
+{
+ Serial.write(StatusByte);
+ Serial.write(Note);
+ Serial.write(Val);
+}
+
+void ProcessPiezo(uint8_t p)
+{
+ uint16_t sensorReading = analogRead(piezo[p]);
+ uint16_t  val;
+
+ if (sensorReading > threshold) 
+ {
+   uint32_t oldcas = millis() + 5;
+   
+   // this part of the code looks complex
+   // explain what you are trying to do?
+   while (analogRead(piezo[p]) > threshold)
+   {
+     if (millis() >= oldcas)
+     {
+       val = analogRead(piezo[p]) / 8;
+     }
+   }
+   
+   MidiTX(144, drumType[p], val);
+   MidiTX(128, drumType[p], val);
+ }
+}
+
+void loop()
+{
+ for (uint8_t i=0; i<MAX_PIEZO; i++)
+   ProcessPiezo(i);
+}
+
---
+++ msp.ino
@@ -0,0 +1,93 @@
+//#define DEBUG_A
+//#define DEBUG_B
+
+#define  midichannel 1;                              // MIDI channel from 0 to 15 (+1 in "real world")
+
+int pinRead;
+byte PadNote[2] = { // MIDI notes from 0 to 127 (Mid C = 60)
+  38, 39
+};         
+        
+int MaxPlayTime[2] = { // Cycles before a 2nd hit is allowed
+  90, 90
+};               
+
+//*******************************************************************************************************************
+// Internal Use Variables
+//*******************************************************************************************************************
+boolean activePad[2] = {
+  0, 0
+};                   // Array of flags of pad currently playing
+int PinPlayTime[2] = {
+  0, 0
+};                     // Counter since pad started to play
+byte status1;
+int pin = 0;
+int hitavg = 0;
+
+
+void setup()
+{
+  Serial.begin(57600);  // connect to the serial port 115200
+  pinMode(A8, INPUT);
+  digitalWrite(A8, HIGH);
+  pinMode(A9, INPUT);
+  digitalWrite(A9, HIGH);
+}
+
+void loop()
+{
+    for(int pin=0; pin <= 1; pin++)                       
+    {
+      boolean on  = false;
+      if(pin == 0)
+         on = digitalRead(A8);
+      else
+         on = digitalRead(A9);
+  
+      if(!on)
+      {
+        if((activePad[pin] == false))
+        {
+            hitavg = 127;
+  
+#ifdef DEBUG_A
+          Serial.println("PADNOTE = = ");
+          Serial.println(PadNote[pin]);
+          Serial.println(pin);
+#endif
+          MIDI_TX(144,PadNote[pin],hitavg); //note on
+          PinPlayTime[pin] = 0;
+          activePad[pin] = true;
+        }
+        else
+        {
+          PinPlayTime[pin] = PinPlayTime[pin] + 1;
+        }
+      }
+      else if((activePad[pin] == true))
+      {
+        PinPlayTime[pin] = PinPlayTime[pin] + 1;
+        if(PinPlayTime[pin] > MaxPlayTime[pin])
+        {
+          activePad[pin] = false;
+          MIDI_TX(128,PadNote[pin],0);
+        }
+      }
+    }
+}
+
+//*******************************************************************************************************************
+// Transmit MIDI Message
+//*******************************************************************************************************************
+void MIDI_TX(byte MESSAGE, byte PITCH, byte VELOCITY)
+{
+    status1 = MESSAGE + midichannel;
+    Serial.write(status1);
+    Serial.write(PITCH);
+    Serial.write(VELOCITY);
+}
+
+
+
+
